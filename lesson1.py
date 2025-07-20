95726fda832d4a243c3f2ed60d216a6c5e450c37
negar-yaghoobi <nyaghoobi77@gmail.com>
2025-07-20 13:13:17 +0330

    add description about streamlit

--- lesson1.py
+++ lesson1.py
@@ -1,1 +1,1 @@
-# streamlit framework : a faster way build and share data apps for ml
+# introduction to streamlit framework : streamlit in an open_source app framework for machine learning and data science projects. it allows you to create web applications for your machine learning and data science projects with simple python scripts. ( a faster way build and share data apps for ml )
