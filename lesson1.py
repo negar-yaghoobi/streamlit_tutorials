2e7757c28e68f2151963fc64f1a6d8c123bf6cdc
negar-yaghoobi <nyaghoobi77@gmail.com>
2025-07-24 21:55:40 +0330

    edit import

--- lesson1.py
+++ lesson1.py
@@ -1,5 +1,5 @@
 # introduction to streamlit framework : streamlit in an open_source app framework for machine learning and data science projects. it allows you to create web applications for your machine learning and data science projects with simple python scripts. ( a faster way build and share data apps for ml )
 
+# import
 # import streamlit
-
 import streamlit as st
